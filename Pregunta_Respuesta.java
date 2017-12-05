98258a3bcb14f7b067c849cf45092191c8905320
Raquel96 <33529458+Raquel96@users.noreply.github.com>
2017-12-05 17:59:21 -0500

    Update Pregunta_Respuesta.java

--- Pregunta_Respuesta.java
+++ Pregunta_Respuesta.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
- * @author leonardo
+ * @author Grupo 1 3A
  */
 public abstract class Pregunta_Respuesta {
     public abstract String pregunta();
